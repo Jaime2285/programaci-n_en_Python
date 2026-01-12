caebd39f08af92ecfcd17472757d23bfbeceabbc
jvelpoz738 <jvelpoz738@g.educaand.es>
2026-01-12 13:34:55 +0100

    he terminado repaso 2

---
+++ repaso2.py
@@ -0,0 +1,28 @@
+a = float("pon un numero")
+if a > 0:
+    print("es numero positivo")
+elif a== 0:
+    print("el numero es 0") 
+else:
+    print("es numero negativo")
+print("fin")
+
+
+
+
+2
+a = int(input("Introduce un año: "))
+
+if (a % 4 == 0 and a % 100 != 0) or (a % 400 == 0):
+    print("El año es bisiesto")
+else:
+    print("El año no es bisiesto")
+
+
+
+
+
+
+
+
+
